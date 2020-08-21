0fabe16d97df3f7332f05853a220db524ea68bb9
Iago Espinoza <iago1501@gmail.com>
2020-08-21 00:00:33 -0300

    :pencil: adding readme.md file

---
+++ README.md
@@ -0,0 +1,47 @@
+Projeto criado para o desafio do módulo 3 da rocketseat, Fundamentos do React Native
+Para este projeto foi utilizado um JSON server para rodar na porta :3333.
+
+O objetivo desta aplicação é realizar a parte de listagem de produtos e a funcionalidade de
+adicionar ao carrinho, incrementar e decrementar a quantidade de items no mesmo, armazenando também em um asyncStorage (cache para mobile)
+de acordo com o layout: https://www.figma.com/file/VgK3hsmyGbqiGu9FdqfUzF/GoMarketplace?node-id=0%3A1
+
+## Scripts disponíveis
+
+Para rodar o projeto basta executar
+
+### `yarn`
+
+Para instalar as dependências da aplicação
+
+### `yarn json-server server.json -p 3333`
+
+Para startar o JSON server (FAKE API)
+
+### `adb -s <device name> reverse tcp:3333 tcp:3333`
+
+Presumindo que a sdk do android já esteja instalada na sua máquina, e que seu dispositivo esteja conectado,
+habilitado modo desenvolvedor e depuração usb, e ao rodar adb devices, o dispositivo esteja como "device"
+
+Este comando irá dizer que a porta 3333 encontrada no seu dispositivo android se equivale a porta 3333 do seu localhost
+
+### `npx react-native react-android`
+
+Roda a aplicação no seu emulador ou no seu dispositivo conectado (por padrão, porta 8081)
+
+### `yarn test`
+
+Para executar os testes necessários para a aplicação ser considerada criada com sucesso
+
+## Fundamentos
+
+Neste projeto foram colocados em prática os conhecimentos adquiridos no módulo 1 do curso de React Native,
+porém, sem muitas novidades de funcionalidades, dado em conta que são bem parecidas com as do React, apenas mudando a forma de escrever Tags pois nao é permitido HTML no React Native
+
+- React-Native;
+- ContextAPI;
+- Typescript;
+- AsyncStorage;
+
+## Observações
+
+Muito perrengue para utilizar o Android Studio, emular pelo celular aparentou ser uma forma mil vezes mais fácil
--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,23 +30,84 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const storagedProducts = await AsyncStorage.getItem(
+        '@GoMarketplace:products',
+      );
+
+      if (storagedProducts) {
+        setProducts([...JSON.parse(storagedProducts)]);
+      }
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  const addToCart = useCallback(
+    async product => {
+      const productExists = products.find(
+        cartProduct => cartProduct.id === product.id,
+      );
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      if (!productExists) {
+        setProducts([...products, { ...product, quantity: 1 }]);
+      } else {
+        setProducts(
+          products.map(cartProduct =>
+            cartProduct.id === product.id
+              ? { ...cartProduct, quantity: cartProduct.quantity + 1 }
+              : { ...cartProduct },
+          ),
+        );
+      }
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      await AsyncStorage.setItem(
+        '@GoMarketplace:products',
+        JSON.stringify(products),
+      );
+    },
+    [products],
+  );
+
+  const increment = useCallback(
+    async id => {
+      const newProducts = products.map(product =>
+        product.id === id
+          ? { ...product, quantity: product.quantity + 1 }
+          : { ...product },
+      );
+
+      setProducts(newProducts);
+      await AsyncStorage.setItem(
+        '@GoMarketplace:products',
+        JSON.stringify(newProducts),
+      );
+    },
+    [products],
+  );
+
+  const decrement = useCallback(
+    async id => {
+      const productIndex = products.findIndex(product => product.id === id);
+
+      const { quantity } = products[productIndex];
+
+      const newProducts =
+        quantity === 1
+          ? products.filter(product => product.id !== id)
+          : products.map(product =>
+              product.id === id
+                ? { ...product, quantity: product.quantity - 1 }
+                : { ...product },
+            );
+
+      setProducts(newProducts);
+      await AsyncStorage.setItem(
+        '@GoMarketplace:products',
+        JSON.stringify(newProducts),
+      );
+    },
+    [products],
+  );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
